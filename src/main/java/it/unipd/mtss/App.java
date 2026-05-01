7450fb22ac40a71099f8f93d42231867f0dba361
Luca Stangherlin <sta.luca05@gmail.com>
2026-05-01 11:10:23 +0200

    risoluzione configurazione build close #5 #3

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -1,13 +1,16 @@
+////////////////////////////////////////////////////////////////////
+// Yi Ming Ye 2137995
+// Luca Stangherlin 2137996
+////////////////////////////////////////////////////////////////////
 package it.unipd.mtss;
 
-/**
- * Hello world!
- *
- */
-public class App 
+
+public final class App
 {
+    private App(){}
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
     }
+
 }
