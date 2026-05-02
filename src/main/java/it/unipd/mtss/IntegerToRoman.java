07383fbc4509cd420f830cb97871c2dcdd7bdbc3
Yi Ming Ye <yeyiming05@gmail.com>
2026-05-02 13:41:05 +0200

    conversione primi dieci numeri close #17

--- src/main/java/it/unipd/mtss/IntegerToRoman.java
+++ src/main/java/it/unipd/mtss/IntegerToRoman.java
@@ -7,20 +7,31 @@ package it.unipd.mtss;
 public final class IntegerToRoman {
     private IntegerToRoman(){}
     public static String convert(int number){
-        if(number<1||number>6){
+        if(number<1||number>10){
             throw new IllegalArgumentException("Numero non valido: " + number);
         }
         StringBuilder result = new StringBuilder();
         if (number>3)
         {
-            if(number==4)
+            if(number==10)
+            {
+                result.append("X");
+                number-=10;
+            }
+            else if(number == 9)
+            {
+                result.append("IX");
+                number-=9;
+            }
+            else if(number==4)
             {
                 result.append("IV");
+                number-=4;
             }
-            else{
+            else if (number%5>=0){
                 result.append("V");
+                number-=5;
             }
-            number-=5;
         }
         for (int i=0; i<number;i++)
         {
