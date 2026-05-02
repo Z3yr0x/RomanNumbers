2a6d088716606de82eb295bae04492fe7130c7b4
Luca Stangherlin <sta.luca05@gmail.com>
2026-05-02 13:30:17 +0200

    test conversione 10

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -13,6 +13,10 @@ public class IntegerToRomanTest {
         assertEquals("IV", IntegerToRoman.convert(4));
         assertEquals("V", IntegerToRoman.convert(5));
         assertEquals("VI", IntegerToRoman.convert(6));
+        assertEquals("VII", IntegerToRoman.convert(7));
+        assertEquals("VIII", IntegerToRoman.convert(8));
+        assertEquals("IX", IntegerToRoman.convert(9));
+        assertEquals("X", IntegerToRoman.convert(10));
     }
 
     @Test
@@ -30,6 +34,6 @@ public class IntegerToRomanTest {
     @Test
     void testLimite() {
         assertThrows(IllegalArgumentException.class,
-                () -> IntegerToRoman.convert(7));
+                () -> IntegerToRoman.convert(11));
     }
 }
