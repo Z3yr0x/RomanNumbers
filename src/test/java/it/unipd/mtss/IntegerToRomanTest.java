d98bd059744f37244b2ec48a835dc2352152720a
Yi Ming Ye <yeyiming05@gmail.com>
2026-05-03 12:33:12 +0200

    test conversione 100

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -7,9 +7,13 @@ public class IntegerToRomanTest {
 
     @Test
     void testIntegerToRoman() {
-        int[] inputs =      {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,35,40,41,42,43,44,45,46,47,48,49,50};
-        String[] previsti = {"I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX","XXX","XXXV","XL","XLI","XLII","XLIII","XLIV","XLV","XLVI","XLVII","XLVIII","XLIX","L"};
-
+        int[] inputs = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,30,35,40,41,42,43,44,45,46,47,48,49,50,51,60,70,75,80,90,100};
+        String[] previsti = {
+                "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X",
+                "XI", "XII", "XIII", "XIV", "XV", "XVI", "XVII", "XVIII", "XIX", "XX",
+                "XXX", "XXXV", "XL", "XLI", "XLII", "XLIII", "XLIV", "XLV", "XLVI", "XLVII",
+                "XLVIII", "XLIX", "L", "LI", "LX", "LXX", "LXXV", "LXXX", "XC", "C"
+        };
         for (int i = 0; i < inputs.length; i++) {
             assertEquals(previsti[i], IntegerToRoman.convert(inputs[i]));
         }
@@ -34,7 +38,7 @@ public class IntegerToRomanTest {
 
     @Test
     void testLimite() {
-        int input = 51;
+        int input = 101;
 
         assertThrows(IllegalArgumentException.class,
                 () -> IntegerToRoman.convert(input));
