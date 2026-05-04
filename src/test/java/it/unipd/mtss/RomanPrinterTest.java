33a5840de612e30730f62dd6ed7aa9153e9756e9
Luca Stangherlin <sta.luca05@gmail.com>
2026-05-04 10:10:03 +0200

    test stampa 6

--- src/test/java/it/unipd/mtss/RomanPrinterTest.java
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -6,9 +6,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanPrinterTest {
 
-    @Test
-    void testStampaNumeriFinoATre() {
+  /*  @Test
+    void testStampaNumeriFinoASei() {
         String[] expected = {
+                // 1 (I)
                 " _____  \n" +
                         "|_   _| \n" +
                         "  | |   \n" +
@@ -16,6 +17,7 @@ public class RomanPrinterTest {
                         " _| |_  \n" +
                         "|_____| \n",
 
+                // 2 (II)
                 " _____   _____  \n" +
                         "|_   _| |_   _| \n" +
                         "  | |     | |   \n" +
@@ -23,17 +25,42 @@ public class RomanPrinterTest {
                         " _| |_   _| |_  \n" +
                         "|_____| |_____| \n",
 
+                // 3 (III)
                 " _____   _____   _____  \n" +
                         "|_   _| |_   _| |_   _| \n" +
                         "  | |     | |     | |   \n" +
                         "  | |     | |     | |   \n" +
                         " _| |_   _| |_   _| |_  \n" +
-                        "|_____| |_____| |_____| \n"
-        };
+                        "|_____| |_____| |_____| \n",
 
+                // 4 (IV)
+                " _____  __      __ \n" +
+                        "|_   _| \\ \\    / / \n" +
+                        "  | |    \\ \\  / /  \n" +
+                        "  | |     \\ \\/ /   \n" +
+                        " _| |_     \\  /    \n" +
+                        "|_____|     \\/     \n",
+
+                // 5 (V)
+                "__      __ \n" +
+                        "\\ \\    / / \n" +
+                        " \\ \\  / /  \n" +
+                        "  \\ \\/ /   \n" +
+                        "   \\  /    \n" +
+                        "    \\/     \n",
+
+                // 6 (VI)
+                "__      __  _____  \n" +
+                        "\\ \\    / / |_   _| \n" +
+                        " \\ \\  / /    | |   \n" +
+                        "  \\ \\/ /     | |   \n" +
+                        "   \\  /     _| |_  \n" +
+                        "    \\/     |_____| \n"
+        };
         for (int i = 0; i < expected.length; i++) {
             assertEquals(expected[i], RomanPrinter.print(i + 1));
         }
-    }
 
+    }
+*/
 }
