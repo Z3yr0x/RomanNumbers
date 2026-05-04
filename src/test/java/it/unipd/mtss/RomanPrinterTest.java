74d01b3a5ad03fb8fd3a2baf93ec58bdadd7d0ac
Luca Stangherlin <sta.luca05@gmail.com>
2026-05-04 09:29:22 +0200

    test stampa 3

---
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,43 @@
+package it.unipd.mtss;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class RomanPrinterTest {
+    /*
+    @Test
+    void testStampaNumeriFinoATre() {
+        int[] inputs = {1,2,3};
+
+        String[] expectedOutputs = {
+                """
+             _____
+            |_   _|
+              | |
+              | |
+             _| |_
+            |_____|""",
+
+                """
+             _____   _____
+            |_   _| |_   _|
+              | |     | |
+              | |     | |
+             _| |_   _| |_
+            |_____| |_____|""",
+
+                """
+             _____   _____   _____
+            |_   _| |_   _| |_   _|
+              | |     | |     | |
+              | |     | |     | |
+             _| |_   _| |_   _| |_
+            |_____| |_____| |_____|"""
+        };
+
+        for (int i = 0; i < inputs.length; i++) {
+            assertEquals(expectedOutputs[i], RomanPrinter.print(inputs[i]));
+        }
+    }*/
+}
