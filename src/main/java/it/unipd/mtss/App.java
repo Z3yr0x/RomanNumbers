08a60604c6fec33491154f75b06042eb74baff8a
Yi Ming Ye <yeyiming05@gmail.com>
2026-05-06 10:26:18 +0200

    risoluzione chiusura scanner close #32

--- src/main/java/it/unipd/mtss/App.java
+++ src/main/java/it/unipd/mtss/App.java
@@ -12,9 +12,9 @@ public final class App
     private App(){}
     public static void main( String[] args )
     {
-        Scanner scan = new Scanner(System.in);
-        System.out.println(RomanPrinter.print(Integer.parseInt(scan.nextLine())));
-        scan.close();
+        try (Scanner scan = new Scanner(System.in)) {
+            System.out.println(RomanPrinter.print(Integer.parseInt(scan.nextLine())));
+        }
     }
 
 }
